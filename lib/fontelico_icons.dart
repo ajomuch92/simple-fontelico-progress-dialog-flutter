8ca79e0178f625619de6cf77fa0d9ed91fb94a16
Aarón Montes <aaron@kssco.com>
2021-05-05 20:39:10 -0600

    Formatting icons library file

--- lib/fontelico_icons.dart
+++ lib/fontelico_icons.dart
@@ -7,20 +7,26 @@ class Fontelico {
   static const _kFontFam = 'Fontelico';
 
   /// IconData for threelines icon
-  static const IconData threelines = IconData(0xe830, fontFamily: _kFontFam, fontPackage: 'simple_fontellico_progress_dialog');
+  static const IconData threelines = IconData(0xe830,
+      fontFamily: _kFontFam, fontPackage: 'simple_fontellico_progress_dialog');
 
   /// IconData for multilines icon
-  static const IconData multilines = IconData(0xe831, fontFamily: _kFontFam, fontPackage: 'simple_fontellico_progress_dialog');
+  static const IconData multilines = IconData(0xe831,
+      fontFamily: _kFontFam, fontPackage: 'simple_fontellico_progress_dialog');
 
   /// IconData for refresh icon
-  static const IconData refresh = IconData(0xe832, fontFamily: _kFontFam, fontPackage: 'simple_fontellico_progress_dialog');
+  static const IconData refresh = IconData(0xe832,
+      fontFamily: _kFontFam, fontPackage: 'simple_fontellico_progress_dialog');
 
   /// IconData for hurricane icon
-  static const IconData hurricane = IconData(0xe834, fontFamily: _kFontFam, fontPackage: 'simple_fontellico_progress_dialog');
+  static const IconData hurricane = IconData(0xe834,
+      fontFamily: _kFontFam, fontPackage: 'simple_fontellico_progress_dialog');
 
   /// IconData for iphone icon
-  static const IconData iphone = IconData(0xe838, fontFamily: _kFontFam, fontPackage: 'simple_fontellico_progress_dialog');
+  static const IconData iphone = IconData(0xe838,
+      fontFamily: _kFontFam, fontPackage: 'simple_fontellico_progress_dialog');
 
   /// IconData for phoenix icon
-  static const IconData phoenix = IconData(0xe839, fontFamily: _kFontFam, fontPackage: 'simple_fontellico_progress_dialog');
+  static const IconData phoenix = IconData(0xe839,
+      fontFamily: _kFontFam, fontPackage: 'simple_fontellico_progress_dialog');
 }
